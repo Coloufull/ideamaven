6e9112d4e80efdafb65312d232a6b38834d59a13
Coloufull <15181401624@163.com>
2021-06-26 15:51:16 +0800

    新项目第一次提交

---
+++ src/main/java/com/itschool/maven/Maven_Helloword/App.java
@@ -0,0 +1,13 @@
+package com.itschool.maven.Maven_Helloword;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/itschool/maven/Maven_Helloword/AppTest.java
@@ -0,0 +1,20 @@
+package com.itschool.maven.Maven_Helloword;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
